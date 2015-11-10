503be284e46941c0a5d8a7b3e9c52685c0a7f3c0
donghyuck <andang.son@gmail.com>
2015-11-10 10:23:02 +0900

    Signed-off-by: donghyuck <andang.son@gmail.com>

--- competency-assessment/src/main/java/com/podosoftware/competency/spring/config/WebCompetencyAccessmentConfig.java
+++ competency-assessment/src/main/java/com/podosoftware/competency/spring/config/WebCompetencyAccessmentConfig.java
@@ -6,8 +6,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {
-		"com.podosoftware.competency.spring.controller"})
-public class WebCompetencyAccessmentConfig  {
+@ComponentScan(basePackages = {"com.podosoftware.competency.spring.controller"})
+public class WebCompetencyAccessmentConfig {
 
 }
