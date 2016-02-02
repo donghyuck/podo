21793522d3d93c654df80dc6a176b17cec8ced02
donghyuck <andang.son@gmail.com>
2016-02-02 16:48:40 +0900

    Signed-off-by: donghyuck <andang.son@gmail.com>

---
+++ competency-assessment/src/main/java/com/podosoftware/competency/job/JobLevel.java
@@ -0,0 +1,146 @@
+package com.podosoftware.competency.job;
+
+import java.io.Serializable;
+
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import architecture.common.cache.CacheSizes;
+import architecture.common.cache.Cacheable;
+import architecture.common.model.ModelObject;
+
+public class JobLevel implements Cacheable, ModelObject {
+
+	private Long jobLevelId;
+
+	private Long jobId;
+
+	private String name;
+
+	private String description;
+
+	private Integer level;
+
+	private Integer minWorkExperienceYear;
+
+	private Integer maxWorkExperienceYear;
+
+	public JobLevel() {
+		this.jobLevelId = -1L;
+		this.jobId = -1L;
+		this.name = null;
+		this.description = null;
+		this.level = 0;
+		this.minWorkExperienceYear = 0;
+		this.maxWorkExperienceYear = 0;
+	}
+
+	
+	
+	public JobLevel(Long jobLevelId, Long jobId, String name, String description, Integer level,
+			Integer minWorkExperienceYear, Integer maxWorkExperienceYear) {
+		this.jobLevelId = jobLevelId;
+		this.jobId = jobId;
+		this.name = name;
+		this.description = description;
+		this.level = level;
+		this.minWorkExperienceYear = minWorkExperienceYear;
+		this.maxWorkExperienceYear = maxWorkExperienceYear;
+	}
+
+
+
+	public Long getJobLevelId() {
+		return jobLevelId;
+	}
+
+	public void setJobLevelId(Long jobLevelId) {
+		this.jobLevelId = jobLevelId;
+	}
+
+	public Long getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(Long jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Integer getLevel() {
+		return level;
+	}
+
+	public void setLevel(Integer level) {
+		this.level = level;
+	}
+
+	public Integer getMinWorkExperienceYear() {
+		return minWorkExperienceYear;
+	}
+
+	public void setMinWorkExperienceYear(Integer minWorkExperienceYear) {
+		this.minWorkExperienceYear = minWorkExperienceYear;
+	}
+
+
+
+	public Integer getMaxWorkExperienceYear() {
+		return maxWorkExperienceYear;
+	}
+
+	public void setMaxWorkExperienceYear(Integer maxWorkExperienceYear) {
+		this.maxWorkExperienceYear = maxWorkExperienceYear;
+	}
+
+	@JsonIgnore
+	public int getCachedSize() {
+		int totalSize = CacheSizes.sizeOfLong() + CacheSizes.sizeOfLong() + CacheSizes.sizeOfInt()
+				+ CacheSizes.sizeOfInt() + CacheSizes.sizeOfInt() + CacheSizes.sizeOfString(name)
+				+ CacheSizes.sizeOfString(description);
+		return totalSize;
+	}
+
+	@JsonIgnore
+	public Serializable getPrimaryKeyObject() {
+		return jobLevelId;
+	}
+
+	@JsonIgnore
+	public void setModelObjectType(int modelObjectType){
+		
+	}
+	
+	public int getModelObjectType() {
+		return 61;
+	}
+
+	public int hashCode() {
+		return new HashCodeBuilder().append(jobLevelId).toHashCode();
+	}
+
+	public boolean equals(Object obj) {
+		if( obj instanceof JobLevel ){
+			if( this.jobLevelId == ((JobLevel)obj).getJobLevelId() )
+				return true;
+			return false;
+		}
+		return super.equals(obj);
+	}
+}
